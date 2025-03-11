67b869acdbbf29ff4be73a057467c1751275d357
chalshik <nurmuhammedkalmamatov06@gmail.com>
2025-03-11 16:54:47 +0600

    Generics

---
+++ src/GenericAssignment.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class GenericAssignment {
+    public static class Storage<T>{
+        ArrayList<T> arrayList = new ArrayList<>();
+        public void add(T t){
+            arrayList.add(t);
+        }
+        public void remove(T t){
+            arrayList.remove(t);
+        }
+        public void display(){
+            for(T item:arrayList){
+                System.out.println(item);
+            }
+        }
+
+        public String toString() {
+            return " array"+ arrayList;
+        }
+    }
+
+    public static void main(String[] args) {
+        Storage<String> s = new Storage();
+        s.add("String");
+        s.display();
+        s.remove("String");
+        System.out.println(s);
+
+    }
+
+}
